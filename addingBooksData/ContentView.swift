b23adb72f73999798143ef37478eea43f716f949
Adii78560 <raiaditya145@gmail.com>
2024-06-12 15:30:52 +0530

    finalCommit

---
+++ addingBooksData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  addingBooksData
+//
+//  Created by Adii I on 12/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            AddingBooksData()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
